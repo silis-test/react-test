c426634cc462d5359b0e9f589abae9fec6bf850b
silis-test <skleemoff@gmail.com>
2023-07-21 12:21:33 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to='/react-test'>Home</Link>
+        <Link to='/new'>New</Link>
+        <Routes>
+          <Route path='/react-test' element={<div>Homepage</div>} />
+          <Route path='/new' element={<div>New</div>} />
+        </Routes>
       </header>
     </div>
   );
